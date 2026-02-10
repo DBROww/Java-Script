add2df47c29d2c033527fdffa01c845f2c7d8858
DBROww <davigmene@gmail.com>
2026-02-10 15:34:33 -0300

    new activities

---
+++ javascript/operadores e funções/atividades.js
@@ -0,0 +1,42 @@
+// Função hora atual
+function horaAtual() {
+    let data = new Date();
+    console.log(`Hora atual: ${String(data.getHours()).padStart(2, '0')}:${String(data.getMinutes()).padStart(2, '0')}:${String(data.getSeconds()).padStart(2, '0')}`);
+}
+
+horaAtual();
+
+console.log("\n"); // Pula uma linha
+
+// Função que recebe dois números e mostra soma e média.
+function somaMedia(n1, n2) {
+    console.log(`A soma de ${n1} e ${n2} é igual a: ${n1 + n2}`);
+    console.log(`A média de ${n1} e ${n2} é igual a: ${(n1 + n2) / 2}`);
+}
+
+let numero1 = Number(prompt("Digite um número"));
+let numero2 = Number(prompt("Digite outro número"));
+
+somaMedia(numero1, numero2);
+
+
+
+console.log("\n"); // Pula uma linha
+
+// Função que recebe nome e mostra sua quantidade de letras e o mostra em maiúsculo.
+function quantAndMaiusculo(nome) {
+    console.log(`Quantidade de letras do nome ${nome}:`, nome.length);
+    console.log(`${nome} em maiúsculo: `, nome.toUpperCase());
+}
+
+quantAndMaiusculo(prompt("Digite um nome:"));
+
+console.log("\n"); // Pula uma linha
+
+// Função que verifica se a palavra HTML está no texto
+function palavraPresente(frase) {
+    console.log(frase.includes("HTML"));
+}
+
+palavraPresente("Eu ja estudei hTmL".toUpperCase()); // Deixa o texto todo em maiscúlo, pois HTML está em maiúsculo, evitando erros. 
+
--- javascript/operadores e funções/funcoes.js
+++ javascript/operadores e funções/funcoes.js
@@ -35,7 +35,7 @@ console.log(imc(64, 1.71));
 console.log("\n");
 
 function imparPar(numero) {
-    if (numero % 2 == 0){
+    if (numero % 2 === 0){
         return(`O número ${numero} é par`);
     }else{
         return (`O número ${numero} é ímpar`);
@@ -43,3 +43,72 @@ function imparPar(numero) {
 }
 
 console.log(imparPar(7));
+
+// Função Data e Hora
+console.log("\n");
+
+let agora = new Date();
+console.log(agora);
+
+function mostrarDataHora() {
+    let data = new Date();
+
+    console.log("Dia:", data.getDate());
+    console.log("Mês:", data.getMonth() + 1);
+    console.log("Ano:", data.getFullYear());
+    console.log("Hora:", data.getHours());
+    console.log("Minutos:", data.getMinutes());
+    
+}
+
+mostrarDataHora();
+
+function mostrarDataHoraPlus() {
+    let data = new Date();
+    console.log(`Hora: ${String(data.getHours()).padStart(2, '0')}:${String(data.getMinutes()).padStart(2, '0')}:${String(data.getSeconds()).padStart(2, '0')} Data: ${String(data.getDate()).padStart(2, '0')}/${String(data.getMonth() + 1).padStart(2, '0')}/${data.getFullYear()}`);
+    
+    //String().padStart(2, '0') --> Função para colocar zero em 2 digitos no começo do número caso o valor não possua dois zeros, Exemplo: 1 -> 01 0 -> 00.
+}
+
+mostrarDataHoraPlus();
+
+// Funções Matemáticas
+console.log("\n")
+
+console.log(Math.PI);
+
+function calcularOperacoes(numero) {
+    console.log("Raiz:", Math.sqrt(numero));
+    console.log("Arredondado:", Math.round(numero));
+    console.log("Para cima:", Math.ceil(numero));
+    console.log("Para baixo:", Math.floor(numero));
+    console.log("Quadrado:", Math.pow(numero, 2));
+    console.log("Valor absoluto:", Math.abs(numero));
+}
+
+calcularOperacoes(7.8);
+
+// Funções String
+
+// 1
+function analisarTexto(texto) {
+    console.log("Tamanho:", texto.length);
+    console.log("Maiúsculo:", texto.toUpperCase());
+    console.log("Minúsculo:", texto.toLowerCase());
+}
+
+analisarTexto("JavaScript");
+
+// 2
+function palavraPresente(frase) {
+    console.log(frase.includes("JavaScript"));
+}
+
+palavraPresente("Eu estudo JavaScript");
+
+// 3
+function adicionaVariavel(nome, curso) {
+    return "Aluno: " + nome + "| Curso: " + curso;
+}
+
+console.log(adicionaVariavel("José", "Front-End"));
