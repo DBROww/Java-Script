fcf52f88ee5f374ab0c258d2e452ae2d08cc2d27
DBROww <davigmene@gmail.com>
2026-03-24 12:01:06 -0300

    edit on line 102 - 144 in atividades.js

--- javascript/DOM/03.03.2026/atividades.js
+++ javascript/DOM/03.03.2026/atividades.js
@@ -3,12 +3,12 @@
 let blueBotao = document.querySelector("#blueBotao"); // Botão para deixar texto Azul
 let redBotao = document.querySelector("#redBotao"); // Botão para deixar texto HTML
 
-blueBotao.addEventListener("click", function() {
-    document.getElementById("titulo").style.color = "blue";   
+blueBotao.addEventListener("click", function () {
+    document.getElementById("titulo").style.color = "blue";
 });
 
-redBotao.addEventListener("click", function() {
-    document.getElementById("titulo").style.color = "red";   
+redBotao.addEventListener("click", function () {
+    document.getElementById("titulo").style.color = "red";
 });
 
 //--------------------------------------------------------------------------------------------------------
@@ -19,7 +19,7 @@ redBotao.addEventListener("click", function() {
 let botao = document.querySelector("#botao");
 let texto = document.querySelector("#texto")
 
-botao.addEventListener("click", function() {
+botao.addEventListener("click", function () {
     if (texto.textContent === "") { // se o texto tiver mudado então:
         texto.textContent = "Texto brabo 2.0 reload pro max ultra hd"; // texto recebe texto original
     } else { // se não
@@ -47,7 +47,7 @@ let clicks = document.querySelector("#clicks"); // Liga a classe <p> para mostra
 let contador = 0; // representa a quantidade de clicks
 
 clicker.addEventListener("click", function () {
-    contador ++; // soma +1 ao contador
+    contador++; // soma +1 ao contador
     clicks.textContent = "Número de clicks: " + contador; // clicks (<p>) recebe contador
 });
 
@@ -58,12 +58,12 @@ clicker.addEventListener("click", function () {
 let item = document.querySelector("#item"); // Recebe o input do HTML
 let adicionar = document.querySelector("#adicionar"); // Liga ao botão HTML
 
-adicionar.addEventListener("click", function() {
+adicionar.addEventListener("click", function () {
 
     let novoItem = document.createElement("li"); // createElement adiciona um elemento (<p>, <ul> <li> etc) a um item
     novoItem.textContent = item.value + " - Excluir"; // novoItem recebe o conteúdo do input HTML
 
-    novoItem.addEventListener("click", function() { // Evento criado para remover item da lista
+    novoItem.addEventListener("click", function () { // Evento criado para remover item da lista
 
         let confirmacao = confirm("Realmente deseja excluir este Item?"); // confirm() pergunta Sim/Não
 
@@ -85,11 +85,11 @@ let user = document.querySelector("#user");
 let botaoSubmit = document.querySelector("#submit");
 let msgSucesso = document.querySelector("#sucesso");
 
-botaoSubmit.addEventListener("click", function() {
+botaoSubmit.addEventListener("click", function () {
     if (user.value.trim() !== "") { // trim() é utilizado para remover o espaço no inicio ou fim de uma frase
         msgSucesso.textContent = "Nome enviado com sucesso!"; // Exemplo trim(): " Js" -> "Js" | wo w" -> "wo w"
         msgSucesso.style.color = "green";
-    }else{
+    } else {
         msgSucesso.textContent = "O campo nome é obrigatório!";
         msgSucesso.style.color = "red";
     }
@@ -100,29 +100,46 @@ botaoSubmit.addEventListener("click", function() {
 // Exercício - Validação de Campos
 
 let email = document.querySelector("#email"); // input email
+let msg = document.querySelector("#msg"); // <p> para reportar erro abaixo do input email
 let senha = document.querySelector("#senha"); // input senha
-let qnt = document.querySelector("#quantidade"); // quantidade caracteres senha
-let validar = document.querySelector("#validar"); // botao validar
-let reportar = document.querySelector("#reportar"); // reportar erro
+let qnt = document.querySelector("#quantidade"); // <p> para exibir quantidade de caracteres da senha
+let validar = document.querySelector("#validar"); // botao para validar email
+let reportar = document.querySelector("#reportar"); // <p> para exibir validação abaixo do botão "validar"
 
-senha.addEventListener("keyup", function () { // Verifica se a senha é fraca ou aceitável
+// Validação da senha:
+senha.addEventListener("keyup", function () {
 
-    if (senha.value.length < 6){ // se for menor do que 6 digitos:
+    let tipoSenha = ""; // Vai guardar se a senha é fraca, aceitável ou forte
+    let cor = ""; // Define a cor do tipoSenha
 
-        qnt.textContent = "Caracteres digitados: " + senha.value.length + " ➔ Senha fraca"; 
-    }else{
-        qnt.textContent = "Caracteres digitados: " + senha.value.length + " ➔ Senha aceitável";
+    if (senha.value.length < 6) { // Se for menor que 6 então será fraca
+
+        tipoSenha = "Senha fraca!";
+        cor = "red";
+
+    } else if (senha.value.length > 10) { // Se for maior que 10 então será forte
+
+        tipoSenha = "Senha forte!";
+        cor = "green";
+
+    } else { // Se for maior de 6 a 10 a senha será aceitável
+
+        tipoSenha = "Senha aceitável!";
+        cor = "orange";
     }
+    // innerHTML é usado para "entender" classes HTML no texto | a tag <span> é usada para alterar a cor somente em uma parte do texto
+    qnt.innerHTML = `Caracteres digitados: ${senha.value.length} ➔ <span style="color: ${cor}">${tipoSenha}</span>`;
 });
 
-validar.addEventListener("click", function() { // Verificar se email contém "@" e "."
+// Validação do email:
+validar.addEventListener("click", function () { // Verificar se email contém "@" e "."
 
-    if (email.value.includes("@") && email.value.includes(".")){ // Se tiver:
+    if (email.value.includes("@") && email.value.includes(".")) { // Se tiver:
         reportar.textContent = "Email válido!";
         reportar.style.color = "green";
-    }else{ // Senão:
-        reportar.textContent = "Email inválido! Precisa conter '@' e '.'";
-        reportar.style.color = "red";
+    } else { // Senão:
+        msg.textContent = "Email inválido! Precisa conter '@' e '.'";
+        msg.style.color = "red";
     }
 }); // Vale ressaltar que o input "@." conta como válido!
 
